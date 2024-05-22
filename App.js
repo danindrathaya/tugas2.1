e417aa1f1317f99c511d5bd2c54fca7ea5bed24d
danindrathaya <162014489+danindrathaya@users.noreply.github.com>
2024-05-22 23:45:20 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hi, this is me - Danindra!</h1>
+      <p>Right now, I am 19 years old and I live in Bandung.</p>
+        <p>
+        My full name is Muhammad Danindra Athaya and I am a digital business student.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
